0d8ddee02a18c5bec9ed99a5da0ca44850bd6eba
Sean Jensen <ItSkolesean@gmail.com>
2021-11-01 19:55:24 +0100

    COMPLETED PART A!

--- Oppgave_2/oppg2.c
+++ Oppgave_2/oppg2.c
@@ -13,29 +13,38 @@ int main(void) {
     if ( pInputFile ) {
         FILE *pOutputFile = fopen(OUTPUT_FILE, "w");
         if ( pOutputFile ) {
-            char current = getc(pInputFile);
             char *szBuffer = (char*) malloc(sizeof(char) * 3);
+            char current;
+            char prev;
+            char* pair;
+            int count = 0;
             if ( szBuffer ) {
-                while(current!= EOF ) {
-                    if ( current == '\n' ) {
+                while((current = fgetc(pInputFile)) != EOF ) {
+                    if ( current == '\n') {
                         continue;
                     }
+                    count++;
+                    if ( count % 2 == 0 ) { // We GOT 2 chars.
+                        pair = combineTwo(prev, current);
+                        printf("%c", hexToInt(pair));
 
-
-                    current = getc(pInputFile);
+                    } else {
+                        prev = current;
+                    }
                 }   // End while getc(pInput);
             } else {// End if szBuffer else
-                perror("Error in opening Outputfile");
+                perror("Error in Malloc szBuffer");
                 return ERROR;
             }   // End else szBuffer;
+
             fclose(pOutputFile);
-        } else { // End If pOutput
+        } else { // End if pOutput
             perror("Error in opening Outputfile");
             return ERROR;
         }   // End else pOutput
 
         fclose(pInputFile);
-    } else {    // End If pInput
+    } else {    // End if pInput
         perror("Error in opening InputFile");
         return ERROR;
     }   // End else pInput
