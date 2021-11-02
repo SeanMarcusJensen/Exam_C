5fe7e93fcf5210b39935a0cdce8cbc5aa10405ca
ITskoleSean <67327992+ITskoleSean@users.noreply.github.com>
2021-11-02 14:50:46 +0100

    Added security for malloc piOCC

--- Oppgave_2/oppg2.c
+++ Oppgave_2/oppg2.c
@@ -8,34 +8,38 @@ int main(void) {
             // Declare Variables.
             char chCurrent, chPrev, chCharacter;
             char* szPair;
-            int *piOcc = (int*) malloc(sizeof(int) * 25); // Count of alfabeth.
             int count = 0;
-            // While File has chars.
-            while((chCurrent = fgetc(pInputFile)) != EOF ) {
-                // I want to skip all if the char is \n, else it will bring problems.
-                if ( chCurrent == '\n') {
-                    continue;
-                }
-                // adding Count to see when we have 2 chars to combine.
-                count++;
-                if ( count % 2 == 0 ) { // We GOT 2 chars.
-                    // Sending in both chars to combine function. and then write it to file.
-                    szPair = combineTwo(chPrev, chCurrent);
-                    chCharacter = hexToInt(szPair);
-                    countOccorances(piOcc ,chCharacter);
-                    fprintf(pOutputFile, "%c", chCharacter);
-                } else {    // End if count % 2 == 0.
-                    chPrev = chCurrent;
-                }   // End else count % 2 == 0
-            }   // End while getc(pInput);
+            int *piOcc = (int*) malloc(sizeof(int) * 25); // Count of alfabeth.
+            if ( piOcc) {
+                // While File has chars.
+                while((chCurrent = fgetc(pInputFile)) != EOF ) {
+                    // I want to skip all if the char is \n, else it will bring problems.
+                    if ( chCurrent == '\n') {
+                        continue;
+                    }
+                    // adding Count to see when we have 2 chars to combine.
+                    count++;
+                    if ( count % 2 == 0 ) { // We GOT 2 chars.
+                        // Sending in both chars to combine function. and then write it to file.
+                        szPair = combineTwo(chPrev, chCurrent);
+                        chCharacter = hexToInt(szPair);
+                        countOccorances(piOcc ,chCharacter);
+                        fprintf(pOutputFile, "%c", chCharacter);
+                    } else {    // End if count % 2 == 0.
+                        chPrev = chCurrent;
+                    }   // End else count % 2 == 0
+                }   // End while getc(pInput);
 
-            // Printing out the output of all chars in the file.
-            printf("Character Count found in file:\n");
-            for ( int i = 0; i <= 25; i++ ) {
-                printf("%c: %i\n", i + 65, piOcc[i]);
-            }
-
-            free(piOcc);
+                // Printing out the output of all chars in the file.
+                printf("Character Count found in file:\n");
+                for ( int i = 0; i <= 25; i++ ) {
+                    printf("%c: %i\n", i + 65, piOcc[i]);
+                }   // End for Printing out.
+                free(piOcc);
+            } else {  // End if piOcc malloc
+                perror("Error in malloc piOcc");
+                return ERROR;
+            } // End else Malloc piOcc
             fclose(pOutputFile);
         } else { // End if pOutput
             perror("Error in opening Outputfile");
